cf2d0b9c079adab6c0c7e886932f77c672e5f3f5
katt.pham <katt.pham@outlook.com>
2019-07-24 12:09:32 -0700

    Actually adding the sql files

---
+++ DB-BoRB/dbo/Tables/dimLUTStatus.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[dimLUTStatus] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Status] NVARCHAR (32) NOT NULL,
+    CONSTRAINT [PK_LUTStatus] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
